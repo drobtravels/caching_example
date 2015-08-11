7055de645621f3863890f3bf88718512aa3b415c
David Roberts <drob@drob.net>
2015-08-10 23:10:25 -0400

    better russian doll fragment caching

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,20 +1,11 @@
 class TopicsController < ApplicationController
 
   def index
-    cache_key = "topic_index/#{Topic.maximum(:updated_at)}"
-    @topics = Rails.cache.fetch(cache_key) do 
-      Rails.logger.info "Querying for topics"
-      topics_with_associations.all
-    end
+    @cache_key = "topic_index/#{Topic.maximum(:updated_at)}"
+    @topics = Topic.all
   end
 
   def show
-    @topic = topics_with_associations.find(params[:id])
-  end
-
-  protected
-
-  def topics_with_associations
-    Topic.includes(:products, posts: :comments)
+    @topic = Topic.find(params[:id])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def posts_cache_key
+    "posts/#{Post.maximum(:updated_at)}"
+  end
+
+  def products_cache_key
+    "products/#{Product.maximum(:updated_at)}"
+  end
 end
